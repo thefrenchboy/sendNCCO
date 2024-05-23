2e5b05ef1201cc53f815a1f663b2a4b0477461f3
thefrenchboy <pro@julien-roche.com>
2024-05-23 12:36:32 +0200

    log beep started

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ app.use(express.json());
 
 app.post('/onEvent', (req, res, next) => {
     if (req.body.status == 'machine' && req.body.sub_state == 'beep_start') {
+        console.log("BEEP STARTED... SEND NCCO !!!")
         const ncco = [{
             action: 'stream',
             streamUrl: 'https://mychezmoi.retool.com/api/file/a257bcf1-6da5-43ee-8371-939189cd96f5'
